ce364449a0be21a3d3c0fb6aca414a8f32ca10cd
fakeha <fakeha.786.momin@gmail.com>
2025-10-06 17:51:09 -0400

    work on comment

--- supabase/migrations/20251003162056_add_donors_adress_columns.sql
+++ supabase/migrations/20251003162056_add_donors_adress_columns.sql
@@ -11,9 +11,3 @@ alter table "public"."donors" add column "profile_photo" text;
 alter table "public"."donors" add column "state" text;
 
 alter table "public"."donors" add column "updated_at" timestamp with time zone default now();
-
-alter table "public"."donors" alter column "first_name" drop not null;
-
-alter table "public"."donors" alter column "last_name" drop not null;
-
-
